1429af70359be623eee15ec623bec72e8ea2f559
Christoffer <christoffer.rosenlind@gmail.com>
2026-02-01 18:03:42 +0100

    Init client

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
+import { Client, Events, GatewayIntentBits } from 'discord.js';
+
 import dotenv from 'dotenv';
 
 dotenv.config();
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, (c) => {
+  console.log(`Logged in as ${c.user.tag}`);
+});
 
 console.log('ATV-bot running...');
